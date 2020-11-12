d01116b00ea19c856410329aaa13289094401498
Michal Juralowicz <caradornian.ajwo@gmail.com>
2020-11-12 10:14:00 +0100

    wrong side AI bug fixed

--- space_game/main.py
+++ space_game/main.py
@@ -8,15 +8,9 @@ from space_game.GameController import GameController
 from space_game.ai_controllers.SBDQNController import SBDQNController
 from space_game.events.KeyPressedEvent import KeyPressedEvent
 from space_game.Config import Config
-from space_game.Player import create_human_player_1, create_player_2
+from space_game.Player import create_human_player_2, create_player_1
 
 logger = logging.getLogger()
-
-
-@dataclass()
-class Assets:
-    pass
-    # ship_1 = pygame.image.load(os.path.join("assets", "ship_green.png"))
 
 
 def main():
@@ -24,13 +18,12 @@ def main():
     clock = pygame.time.Clock()
     config = Config()
     game_controller = GameController(config)
-    player_1, p1_controller = create_human_player_1(config, game_controller.event_manager)
+    player_2, p2_controller = create_human_player_2(config, game_controller.event_manager)
     dqn_model = DQN.load("../trained_models/deepq_breakout.zip")
-    # player_2, p2_controller = create_human_player_2(config, game_controller.event_manager)
-    player_2 = create_player_2(config, game_controller.event_manager)
-    ai_2 = SBDQNController(game_controller.event_manager, config, player_2, dqn_model)
-    game_controller.__add_player__(player_1, p1_controller)
-    game_controller.__add_player__(player_2)
+    player_1 = create_player_1(config, game_controller.event_manager)
+    ai_2 = SBDQNController(game_controller.event_manager, config, player_1, dqn_model)
+    game_controller.__add_player__(player_1)
+    game_controller.__add_player__(player_2, p2_controller)
     game_controller.__add_ai_controller__(ai_2)
     pressed_keys = {}
     while running:
