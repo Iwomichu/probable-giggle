ff6bea98c038366a18987523b230655119837d2b
Michal Juralowicz <caradornian.ajwo@gmail.com>
2020-12-29 19:50:39 +0100

    Refactor: Tournament system refactored

--- tournament_system/TournamentRegister.py
+++ tournament_system/TournamentRegister.py
@@ -1,90 +1,101 @@
-from collections import namedtuple
-from typing import Tuple
+from typing import Dict, List
+from itertools import combinations
 
 import numpy as np
-ModelEntry = namedtuple('Model_with_id', ('model', 'id'))
-RoundResult = namedtuple('Result_of_one_round', ('match_result', 'first_model', 'second_model', 'stats'))
-RankingEntry = namedtuple('Ranking', ('model', 'model_id', 'won_matches', 'lost_matches', 'draws'))
+
+from tournament_system.ModelEntry import ModelEntry, ModelInstance, ModelID
+from tournament_system.RankingEntry import RankingEntry
+from tournament_system.RoundReport import RoundReport, RoundResult
+from tournament_system.RoundStat import RoundStat
+
+
+def one_round(fst_model: ModelEntry, snd_model: ModelEntry) -> RoundReport:
+    match_result = np.random.choice([
+        RoundResult.DRAW, RoundResult.FIRST_PLAYER_WIN, RoundResult.SECOND_PLAYER_WIN
+    ], 1)[0]
+    stats = RoundStat()
+    return RoundReport(match_result, fst_model, snd_model, stats)
+
+
+def calculate_ranking_points(ranking_entry: RankingEntry) -> int:
+    return ranking_entry.wins_count * 3 + ranking_entry.draws_count
 
 
 class TournamentRegister(object):
-    """
-    tournament class: everywhere we have 'model', we have model like DQN. Other cases we have namedtuple Model. It
-    is marked in functions.
-    """
-
-    def __init__(self, length_of_learning: int, rounds_in_the_game: int, *models: ModelEntry):
-        self.ranking_table = []
-        self.results_of_tournament = []
+    def __init__(self, length_of_learning: int, rounds_in_the_game: int, *models: ModelInstance):
+        self.ranking_table: List[RankingEntry] = []
+        self.results_of_tournament: List[RoundReport] = []
         self.length_of_learning = length_of_learning
         self.rounds_in_the_game = rounds_in_the_game
-        if len(models) == 0:
-            self.models = []
-        else:
-            self.models = [ModelEntry(model[0], model[1]) for model in models]
+        self.models: Dict[ModelID, ModelEntry] = {idx: ModelEntry(idx, model) for idx, model in enumerate(models)}
+        self.next_model_id = len(self.models)
         print(self.models)
 
-    def add_model(self, model: ModelEntry) -> None:
-        for model_ in self.models:
-            if model_.model == model[0]:
-                raise Exception("model already exist!")
-            elif model_.id == model[1]:
-                raise Exception("this id is used by other model!")
-        self.models.append(ModelEntry(model[0], model[1]))
+    def add_model(self, model: ModelInstance) -> None:
+        if model in self.models:
+            raise Exception("model already exist!")
+        self.models[self.next_model_id] = (ModelEntry(self.next_model_id, model))
+        self.next_model_id += 1
         print(self.models)
 
-    def remove_model_by_id(self, id_to_remove: str) -> None:
-        for model_ in self.models:
-            if model_.id == id_to_remove:
-                self.models.remove(model_)
+    def remove_model_by_id(self, id_to_remove: ModelID) -> None:
+        del self.models[id_to_remove]
         print(self.models)
 
-    def remove_model_by_model(self, model):
-        for model_ in self.models:
+    def remove_model_by_model(self, model: ModelInstance) -> None:
+        for idx, model_ in self.models.items():
             if model_.model == model:
-                self.models.remove(model_)
+                del self.models[idx]
+                break
         print(self.models)
 
-    def one_round(self, fst_model: ModelEntry, snd_model: ModelEntry) -> RoundResult:
-        match_result = np.random.choice(["draw", fst_model.id, snd_model.id], 1)[0]
-        stats = 0
-        return RoundResult(match_result, fst_model, snd_model, stats)
-
-    def tournament(self):
-        n_players = len(self.models)
-        games = [(self.models[player1_index], self.models[player2_index])\
-                 for player1_index in range(n_players) for player2_index in range(player1_index+1, n_players)]
+    def tournament(self) -> None:
+        games = combinations(self.models.values(), 2)
         for match in games:
-            self.results_of_tournament.append(self.one_round(match[0], match[1]))
-        for i in self.results_of_tournament:
-            print(i.match_result)
+            self.results_of_tournament.append(one_round(match[0], match[1]))
+        for result in self.results_of_tournament:
+            print(result)
 
-    def get_wins(self, model_id):
-        won_games = 0
-        for match in self.results_of_tournament:
-            if match.match_result == model_id:
-                won_games += 1
-        return won_games
+    def get_wins(self, model_id: ModelID) -> int:
+        wins = [
+            result
+            for result in self.results_of_tournament
+            if (result.first_model.id == model_id and result.result == RoundResult.FIRST_PLAYER_WIN) or
+               (result.second_model.id == model_id and result.result == RoundResult.SECOND_PLAYER_WIN)
+        ]
+        return len(wins)
 
-    def get_loses(self, model_id):
-        lost_games = 0
-        for match in self.results_of_tournament:
-            if (match.first_model.id == model_id or match.second_model.id == model_id) and (match.match_result != model_id and match.match_result != 'draw'):
-                lost_games += 1
-        return lost_games
+    def get_loses(self, model_id: ModelID) -> int:
+        losses = [
+            result
+            for result in self.results_of_tournament
+            if (result.second_model.id == model_id and result.result == RoundResult.FIRST_PLAYER_WIN) or
+               (result.first_model.id == model_id and result.result == RoundResult.SECOND_PLAYER_WIN)
+        ]
+        return len(losses)
 
-    def get_draws(self, model_id):
-        draws = 0
-        for match in self.results_of_tournament:
-            if match.match_result == 'draw' and (match.first_model.id == model_id or match.second_model.id == model_id):
-                draws += 1
-        return draws
+    def get_draws(self, model_id: ModelID) -> int:
+        draws = [
+            result
+            for result in self.results_of_tournament
+            if (result.result == RoundResult.DRAW and result.second_model.id == model_id and result.first_model.id)
+        ]
+        return len(draws)
 
-    def ranking(self):
-        for model_ in self.models:
+    def ranking(self) -> None:
+        for model_ in self.models.values():
             self.ranking_table.append(
-                RankingEntry(model_.model, model_.id, self.get_wins(model_.id), self.get_loses(model_.id), self.get_draws(model_.id)))
-        self.ranking_table.sort(key=lambda x: x.won_matches * 3 + x.draws)
+                RankingEntry(model_, self.get_wins(model_.id), self.get_loses(model_.id), self.get_draws(model_.id)))
+        self.ranking_table.sort(key=calculate_ranking_points)
         for position in reversed(self.ranking_table):
             print(position)
 
+
+if __name__ == '__main__':
+    dqn = "dummy dqn"
+    cdqn = "dummy cdqn"
+    cnn = "dummy cnn"
+
+    tournament = TournamentRegister(100, 10, dqn, cdqn, cnn)
+    tournament.tournament()
+    tournament.ranking()
