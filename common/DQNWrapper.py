d2710dd6413b17cc7e9be81cc29002ceb0d556e1
Iwomichu <caradornian.ajwo@gmail.com>
2021-01-07 10:28:39 +0100

    Bugfix: Post-merge bugfixes

--- common/DQNWrapper.py
+++ common/DQNWrapper.py
@@ -12,11 +12,11 @@ from space_game.ai.AIAction import AIAction
 class DQNWrapper(TorchNNModuleInterface):
     def __init__(self, module: torch.nn.Module):
         super(DQNWrapper, self).__init__(module)
-        self.module = module
+        self.module = module.cpu()
         self.state = torch.zeros((1, 3, 64, 64))
 
     def predict(self, observation: gym.spaces.Box) -> AIAction:
-        processed_observation = process_observation(observation)
+        processed_observation = process_observation(observation).cpu()
         self.state = torch.cat((self.state[:, 1:, :, :], processed_observation.unsqueeze(0)), dim=1)
         raw_action = self.module(self.state).max(1)[1].view(1, 1)
         return EnvironmentActionToAIActionMapping.get(EnvironmentAction(raw_action.item()), AIAction.StandStill)
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def space_game_with_c_dqn():
     p2, p2_controller = create_human_player_2(game_config, game.game_controller.event_manager)
     c_dqn_controller = CDQNController(game.game_controller.event_manager, game_config,
                                       p1, p2, Side.UP, game.game_controller.screen,
-                                      dqn_path=SAVED_MODELS_DIRECTORY / "CustomDQN_19-02-09_06-01-2021" / "dqn.pt")
+                                      dqn_path=SAVED_MODELS_DIRECTORY / "CustomDQN_09-16-16_07-01-2021" / "dqn.pt")
     game.add_player_1(PlayerTuple(p1, None, c_dqn_controller))
     game.add_player_2(PlayerTuple(p2, p2_controller, None))
     game.start()
@@ -51,5 +51,4 @@ def self_play_dqn_training():
 
 
 if __name__ == '__main__':
-    self_play_dqn_training()
     space_game_with_c_dqn()
