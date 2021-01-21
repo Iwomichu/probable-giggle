aee2640dbf279de45b4c81635b507a2984617787
Iwomichu <caradornian.ajwo@gmail.com>
2021-01-21 22:51:15 +0100

    Hotfix: Downside model action inversion added to CDQNController

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,8 @@
 import gym
 import torch
 
+from space_game.ai.AIAction import AIAction
+
 
 def process_observation(observation: gym.spaces.Box) -> torch.Tensor:
     """
@@ -10,3 +12,27 @@ def process_observation(observation: gym.spaces.Box) -> torch.Tensor:
     :return: Processed observation
     """
     return torch.from_numpy(observation).transpose(0, 2)
+
+
+def inverse_movement(action: AIAction) -> AIAction:
+    """
+    Inverses action along X axis (UP <-> DOWN)
+
+    :param action: Original action
+    :return: Inversed action (UP = DOWN)
+    """
+    inversion_dict = {
+        AIAction.MoveDown: AIAction.MoveUp,
+        AIAction.MoveDownShoot: AIAction.MoveUpShoot,
+        AIAction.MoveLeftDown: AIAction.MoveLeftUp,
+        AIAction.MoveLeftDownShoot: AIAction.MoveLeftUpShoot,
+        AIAction.MoveRightDown: AIAction.MoveRightUp,
+        AIAction.MoveRightDownShoot: AIAction.MoveRightUpShoot,
+        AIAction.MoveUp: AIAction.MoveDown,
+        AIAction.MoveUpShoot: AIAction.MoveDownShoot,
+        AIAction.MoveLeftUp: AIAction.MoveLeftDown,
+        AIAction.MoveLeftUpShoot: AIAction.MoveLeftDownShoot,
+        AIAction.MoveRightUp: AIAction.MoveRightDown,
+        AIAction.MoveRightUpShoot: AIAction.MoveRightDownShoot
+    }
+    return inversion_dict.get(action) if action in inversion_dict else action
